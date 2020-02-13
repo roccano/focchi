621fa458f93feee48b254ce8540a97b58cc85438
Rocco <roccangi@gmail.com>
2020-02-13 16:55:08 +0100

    modifiche

--- docs/js/main.js
+++ docs/js/main.js
@@ -108,7 +108,24 @@
           loop: false,
           slidesPerView: 3,
           spaceBetween: 30,
-          centeredSlides: true
+          centeredSlides: true,
+          breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30
+            },
+            // when window width is >= 640px
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 30
+            }
+          }
         });
       };
 
