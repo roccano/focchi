1d357c999ec686d2f1810dadd6582cc3475a1674
Rocco <roccangi@gmail.com>
2020-03-04 17:17:34 +0100

    montaggio group

--- src/js/main.js
+++ src/js/main.js
@@ -241,6 +241,40 @@ import LocomotiveScroll from "locomotive-scroll";
                 }
             }
         })
+        var menu = ['1950 - 1960', '1970', '1975']
+        var swiperGroup = new Swiper('.section--history .swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 40,
+            centeredSlides: true,
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                renderBullet: function(index, className) {
+                    return '<span class="' + className + '">' + (menu[index]) + '</span>';
+                },
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 1,
+                    spaceBetween: 30
+                },
+                // when window width is >= 640px
+                768: {
+                    slidesPerView: 1,
+                    spaceBetween: 150
+                }
+            }
+        })
     };
 
 
